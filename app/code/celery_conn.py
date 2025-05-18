911095e785ecfd958a2e84ceebce04247ebcd8a0
Patrick Meyer <patrick.meyer1992@gmail.com>
2025-05-18 16:06:57 +0000

    fine grain environment variables

--- app/code/celery_conn.py
+++ app/code/celery_conn.py
@@ -4,10 +4,15 @@ import os
 
 rmq_user = os.getenv('RABBITMQ_DEFAULT_USER')
 rmq_passwd = os.getenv('RABBITMQ_DEFAULT_PASS')
+rmq_host = os.getenv('RABBITMQ_HOST')
+rmq_port = os.getenv('RABBITMQ_PORT')
+redis_host = os.getenv('REDIS_HOST')
+redis_port = os.getenv('REDIS_PORT')
+
 celery = Celery(
     'tasks',
-    broker=f'pyamqp://{rmq_user}:{rmq_passwd}@caas-rabbitmq',
-    backend='redis://caas-redis/0',
+    broker=f'pyamqp://{rmq_user}:{rmq_passwd}@{rmq_host}:{rmq_port}',
+    backend=f'redis://{redis_host}:{redis_port}/0',
 )
 
 # celery = Celery('proj',
