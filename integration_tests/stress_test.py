0a5191478bdcf2cc3e1459fd5d7d2fd000779a3e
Patrick Meyer <patrick.meyer1992@gmail.com>
2025-07-02 16:42:22 +0200

    fix typo and num of iteration

--- integration_tests/stress_test.py
+++ integration_tests/stress_test.py
@@ -2,14 +2,14 @@ import requests
 import json
 from time import sleep
 
-# FASTAPI_URL = f"http://api.staging.caas.local:80"
-FASTAPI_URL = f"http://localhost:7001"
+FASTAPI_URL = f"http://api.staging.caas.local:80"
+# FASTAPI_URL = f"http://localhost:7001"
 
 columns = [
-    "sepal_length",
-    "sepal_width",
-    "petal_length",
-    "petal_width",
+    "sepal.length",
+    "sepal.width",
+    "petal.length",
+    "petal.width",
 ]
 
 data = {
@@ -21,10 +21,10 @@ data = {
     "preprocessing_params": {}
 }
 
-iterations = 1
-sleep_time = 1
+iterations = 5000
+sleep_time = 0.1
 for i in range(iterations):
     print(f"Iteration {i + 1}/{iterations}")
-    res = requests.post(f"{FASTAPI_URL}/cluster/", data=data)
+    res = requests.post(f"{FASTAPI_URL}/cluster/", json=data)
     print(res.text)
     sleep(sleep_time)
